294e77dc349acc6cdfbe8b73c30187c74cd7be1d
Vitaliy Hula <gvnhak@gmail.com>
2023-08-10 00:41:14 +0300

    Code refactoring

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 // const array = new Array(1, 2, 3, 5, 20, 42)
 // length - довжиина масиву
 
-
 // console.log(array[0])
 // console.log(array[3])
 // console.log(array[5])
@@ -59,55 +58,37 @@ function getNoteTemplate(title) {
 }
 console.log(inputElement.value)*/
 
-const inputElement = document.getElementById('title')
-const createBtn = document.getElementById('create')
-const listElement = document.getElementById('list')
-const notes = ['зробити дз', 'бути на конгресі'] 
-  
-function render() {
-  listElement.insertAdjacentHTML('beforeend', 
-      `   
-       <li
-       class="list-group-item d-flex justify-content-between align-items-center">
-       <span>${notes[0]}</span>
-            <span>
-            <span class="btn btn-small btn-success">&check;</span>
-            <span class="btn btn-small btn-danger">&times;</span>
-            </span>
-            </li>
-       `
-  )  
-  listElement.insertAdjacentHTML('beforeend', 
-      `   
-       <li
-       class="list-group-item d-flex justify-content-between align-items-center">
-       <span>${notes[1]}</span>
-            <span>
-            <span class="btn btn-small btn-success">&check;</span>
-            <span class="btn btn-small btn-danger">&times;</span>
-            </span>
-            </li>
-       `
-  )   
-}
-render()
-createBtn.onclick = function () { 
-    if (inputElement.value.length === 0) 
-      return  
-  listElement.insertAdjacentHTML('beforeend', 
-      `   
-       <li
-       class="list-group-item d-flex justify-content-between align-items-center">
-       <span>${inputElement.value}</span>
-            <span>
-            <span class="btn btn-small btn-success">&check;</span>
-            <span class="btn btn-small btn-danger">&times;</span>
-            </span>
-            </li>
-       `
-  )   
-  inputElement.value = ''
-}
-// console.log(inputElement.value)*/
+const inputElement = document.getElementById('title');
+const createBtn = document.getElementById('create');
+const listElement = document.getElementById('list');
+const notes = ['зробити дз', 'бути на конгресі'];
 
- 
+function initialRender() {
+  notes.forEach(note => renderRow(note));
+}
+
+function renderRow(title) {
+  listElement.insertAdjacentHTML(
+    'beforeend',
+    `
+      <li
+        class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${title}</span>
+        <span>
+          <span class="btn btn-small btn-success">&check;</span>
+          <span class="btn btn-small btn-danger">&times;</span>
+        </span>
+      </li>
+    `,
+  );
+}
+
+initialRender();
+
+createBtn.onclick = function () {
+  if (inputElement.value.length === 0) return;
+
+  renderRow(inputElement.value);
+  inputElement.value = '';
+};
+// console.log(inputElement.value)*/
